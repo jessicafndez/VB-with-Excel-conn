a46e35e0ba755f57d191e6952a324fe924a5ae97
Jessica <jessicafndez@gmail.com>
2014-09-01 18:27:05 +0200

    Update Invoice_connection.vb

--- Invoice_connection.vb
+++ Invoice_connection.vb
@@ -1,6 +1,5 @@
-Public Class Factures
+Public Class Invoice
 
-    'visualizar tabla'
     Private Sub Button1_Click(ByVal sender As System.Object, _
                 ByVal e As System.EventArgs) Handles Button1.Click
         Try
@@ -9,7 +8,7 @@ Public Class Factures
             Dim MyCommand As System.Data.OleDb.OleDbDataAdapter
             MyConnection = New System.Data.OleDb.OleDbConnection _
              ("provider=Microsoft.Jet.OLEDB.4.0; Data Source=" + _
-            "'C:\Propeti\Facturas.xls';Extended Properties=Excel 8.0;")
+            "'C:\******.xls';Extended Properties=Excel 8.0;")
             MyCommand = New System.Data.OleDb.OleDbDataAdapter _
                 ("select * from [Sheet1$]", MyConnection)
             MyCommand.TableMappings.Add("Table", "TestTable")
